221832dd0f407c80b14285ee1790dc86ef629cd8
PARK-SU <50171671+PARK-SU@users.noreply.github.com>
2021-08-26 04:42:22 +0900

    0.2

--- LR2IR long title fixer (crude).user.js
+++ LR2IR long title fixer (crude).user.js
@@ -1,20 +1,25 @@
 // ==UserScript==
 // @name         LR2IR long title fixer (crude)
 // @namespace    https://github.com/PARK-SU/LR2IR-Long-Title-Fixer
-// @version      0.1.2
+// @version      0.2
 // @description  lr2ir long title fix code
 // @author       nothing (that) created me
 // @match        http://www.dream-pro.info/~lavalse/LR2IR/search.cgi*
 // @grant        none
 // ==/UserScript==
-window.onload = function() {
-    var songTitle = document.querySelectorAll("a, h1");
+window.onload = function () {
+    var songTitle = document.querySelectorAll("td, a, h1");
     for (var i = 0; i < songTitle.length; i++) {
-        var targetHtml = songTitle[i].innerHTML;
-        var targetText = songTitle[i].innerText;
-        if (targetHtml.length >= 100) {
-            var replaceString = targetHtml.replace(targetText, targetText.slice(0, 8) + "..." + targetText.slice(-16));
-            songTitle[i].innerHTML = replaceString;
+        var targetHtml = songTitle[i].innerHTML,
+            targetText = songTitle[i].innerText,
+            stringList = targetHtml.split(" ");
+        if (targetHtml.slice(0, 7) != "<a href") {
+            for (var j = 0; j < stringList.length; j++) {
+                if (stringList[j].length >= 36) {
+                    var replaceString = targetText.replace(stringList[j], stringList[j].slice(0, 8) + "..." + stringList[j].slice(-8));
+                    songTitle[i].innerHTML = replaceString;
+                }
+            }
         }
     }
     console.log('Long title fixed.');
--- README.md
+++ README.md
@@ -3,5 +3,9 @@ crude code
 
 It is [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) script, only work in LR2IR page.
 
-## Todo
-Genre와 Artist에도 적용.
+## Bug
+- http://manbow.nothing.sh/event/event.cgi?~ 형태로 된 링크가 축약되지 않는 문제
+- "備考" 행의 문자열이 이상해짐
+
+## Helped by
+[fsrs](https://github.com/FSRS/)
